dd5330e3aadabdf5abc8197491eb5feddea099fd
Frenz Jay-r Y. Valios <67408909+frenzjay@users.noreply.github.com>
2024-01-25 11:13:50 +0800

    Add files via upload

--- content.js
+++ content.js
@@ -1,32 +1,33 @@
 function translateWords() {
-    var bodyText = document.body.innerText;
-    var matches = bodyText.match(/{(.*?)}/g);
-    if (matches) {
-      matches.forEach(function(match) {
-        chrome.runtime.sendMessage({action: "translateWord", word: match}, function(response) {
-          var elements = document.body.getElementsByTagName('*');
-          for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            for (var j = 0; j < element.childNodes.length; j++) {
-              var node = element.childNodes[j];
-              if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                var replacedText = text.replace(match, response.original);
-                if (replacedText !== text) {
-                  element.replaceChild(document.createTextNode(replacedText), node);
-                }
+  var bodyText = document.body.innerText;
+  var matches = bodyText.match(/{(.*?)}/g);
+  if (matches) {
+    matches.forEach(function(match) {
+      chrome.runtime.sendMessage({action: "translateWord", word: match}, function(response) {
+        var elements = document.body.getElementsByTagName('*');
+        for (var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+          for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+            if (node.nodeType === 3) {
+              var text = node.nodeValue;
+              var replacedText = text.replace(match, response.original);
+              if (replacedText !== text) {
+                var htmlNode = document.createElement('div');
+                htmlNode.innerHTML = response.original;
+                element.replaceChild(htmlNode, node);
               }
             }
           }
-        });
+        }
       });
-    }
+    });
   }
-  
-  translateWords();
-  
-  window.addEventListener('scroll', translateWords);
-  
-  var observer = new MutationObserver(translateWords);
-  observer.observe(document, { childList: true, subtree: true });
-  
+}
+
+translateWords();
+
+window.addEventListener('scroll', translateWords);
+
+var observer = new MutationObserver(translateWords);
+observer.observe(document, { childList: true, subtree: true });
