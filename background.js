94288af6e0e9539e4a66097a59118106b5aae153
Frenz Jay-r Y. Valios <67408909+frenzjay@users.noreply.github.com>
2024-02-10 17:42:55 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.action == "translateWord") {
-        var encodedWord = btoa(request.word);
-        fetch('https://frenzvalios.com/api/occultext/translate/apikey/61135?gibberish=' + encodedWord)
-          .then(response => response.json())
-          .then(data => {
-            sendResponse({original: data.original});
-          });
-        return true;
-      }
+  function(request, sender, sendResponse) {
+    if (request.action == "translateWord") {
+      var encodedWord = btoa(request.word);
+      fetch('https://frenzvalios.com/api/occultext/translate/apikey/61135?gibberish=' + encodedWord)
+        .then(response => response.json())
+        .then(data => {
+          sendResponse({original: data.original});
+        })
+        .catch(error => {
+          console.error("error:", error); 
+          sendResponse({});
+        });
+      return true;
     }
-  );
-  
+  }
+);
--- content.js
+++ content.js
@@ -1,24 +1,29 @@
 function translateWords() {
   var bodyText = document.body.innerText;
   var matches = bodyText.match(/{(.*?)}/g);
+
   if (matches) {
     matches.forEach(function(match) {
       chrome.runtime.sendMessage({action: "translateWord", word: match}, function(response) {
-        var elements = document.body.getElementsByTagName('*');
-        for (var i = 0; i < elements.length; i++) {
-          var element = elements[i];
-          for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
-            if (node.nodeType === 3) {
-              var text = node.nodeValue;
-              var replacedText = text.replace(match, response.original);
-              if (replacedText !== text) {
-                var htmlNode = document.createElement('div');
-                htmlNode.innerHTML = response.original;
-                element.replaceChild(htmlNode, node);
+        if (response && response.original) { 
+          var elements = document.body.getElementsByTagName('*');
+          for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            for (var j = 0; j < element.childNodes.length; j++) {
+              var node = element.childNodes[j];
+              if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(match, response.original);
+                if (replacedText !== text) {
+                  var htmlNode = document.createElement('div');
+                  htmlNode.innerHTML = response.original;
+                  element.replaceChild(htmlNode, node);
+                }
               }
             }
           }
+        } else {
+          console.error('err: ', match); 
         }
       });
     });
@@ -30,4 +35,4 @@ translateWords();
 window.addEventListener('scroll', translateWords);
 
 var observer = new MutationObserver(translateWords);
-observer.observe(document, { childList: true, subtree: true });
+observer.observe(document, { childList: true, subtree: true }); 
