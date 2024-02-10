4eac809625fcd2158459fc3cf44031b2ce5b814f
Frenz Jay-r Y. Valios <67408909+frenzjay@users.noreply.github.com>
2024-02-10 17:18:04 +0800

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.action == "translateWord") {
         var encodedWord = btoa(request.word);
-        fetch('https://frenzvalios.com/api/gibberish/translate/apikey/1122007?gibberish=' + encodedWord)
+        fetch('https://frenzvalios.com/api/occultext/translate/apikey/61135?gibberish=' + encodedWord)
           .then(response => response.json())
           .then(data => {
             sendResponse({original: data.original});
